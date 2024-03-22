07329b6895412a1f85d1518798cd6e8e0fea99d5
vivsoliveira <victoriaof@al.insper.edu.br>
2024-03-22 17:45:54 -0300

    c

--- client/aplicacao_client.py
+++ client/aplicacao_client.py
@@ -1,26 +1,10 @@
-#####################################################
-# Camada Física da Computação
-#Carareto
-#11/08/2022
-#Aplicação
-####################################################
-
-
-#esta é a camada superior, de aplicação do seu software de comunicação serial UART.
-#para acompanhar a execução e identificar erros, construa prints ao longo do código! 
-
-
+import sys
 from enlace import *
 import time
 import numpy as np
 import random
 import datetime
-
-# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
-#   para saber a sua porta, execute no terminal :
-#   python -m serial.tools.list_ports
-# se estiver usando windows, o gerenciador de dispositivos informa a porta
-
+from crc import Calculator, Crc16, Crc8
 
 serialName = "COM5"                  # Windows(variacao de)
 
@@ -34,23 +18,6 @@ def divide_pacotes(txBuffer):
 
     return pacotes, len(pacotes)
 
-# num e o numero de pacote atual
-# len(dados) e o tamanho do pacote
-def datagrama(tipo, num, total_pacotes, dados, numero_img):
-    ceop = b'\xAA\xBB\xAA\xBB'
-    if tipo == 1: # chamado do cliente enviado ao servidor convidando-o para a transmissão
-        head = b'\x01\xFF'+ (total_pacotes).to_bytes(1, 'big') + (numero_img).to_bytes(1, 'big') + b'\x00\x00\x00\x00\x00\x00'
-        dtg = head+ceop
-    elif tipo == 3: # envio de pacotes (mostra x de y pacotes enviados)
-        head = b'\x03'+ (num).to_bytes(1, 'big') + (total_pacotes).to_bytes(1, 'big') + (len(dados)).to_bytes(1, 'big')+ b'\x00\x00\x00\x00\x00\x00'
-        payload_1 = dados
-        dtg = head+payload_1+ceop
-    elif tipo == 5: # mensagem de time out
-        head = b'\x05\x00'+ b'\x00\x00\x00\x00\x00\x00\x00\x00'
-        dtg = head+ceop
-
-    return dtg
-
 def escrever_log(mensagem, nome_arquivo="log.txt"):
     """
     Escreve uma mensagem de log com timestamp em um arquivo especificado.
@@ -59,22 +26,51 @@ def escrever_log(mensagem, nome_arquivo="log.txt"):
     with open(nome_arquivo, "a") as arquivo_log:
         arquivo_log.write(f"[{timestamp}] {mensagem}\n")
 
+def calculate_crc16(data):
+    crc = 0xFFFF  # Valor inicial do CRC
+    poly = 0x1021  # Polinômio CRC-16 (usando o polinômio comum para CRC-16)
+
+    for byte in data:
+        crc ^= (byte << 8)
+        for _ in range(8):
+            if crc & 0x8000:
+                crc = (crc << 1) ^ poly
+            else:
+                crc <<= 1
+            crc &= 0xFFFF  # Garante que o CRC seja de 16 bits
+
+    # Retorna os bytes do CRC em formato big-endian
+    crc_bytes = crc.to_bytes(2, 'big')
+    return crc_bytes
+
 def main():
     try:
-        print("Iniciou o main")
-        #declaramos um objeto do tipo enlace com o nome "com". Essa é a camada inferior à aplicação. Observe que um parametro
-        #para declarar esse objeto é o nome da porta.
-        com1 = enlace(serialName)
+        calculator = Calculator(Crc8.CCITT)
+        # num e o numero de pacote atual
+        # len(dados) e o tamanho do pacote
+        def datagrama(tipo, num, total_pacotes, dados, numero_img):
+            ceop = b'\xAA\xBB\xAA\xBB'
+            if tipo == 1: # chamado do cliente enviado ao servidor convidando-o para a transmissão
+                head = b'\x01\xFF'+ (total_pacotes).to_bytes(1, 'big') + (numero_img).to_bytes(1, 'big') + b'\x00\x00\x00\x00\x00\x00'
+                dtg = head+ceop
+            elif tipo == 3: # envio de pacotes (mostra x de y pacotes enviados)
+                head = b'\x03'+ (num).to_bytes(1, 'big') + (total_pacotes).to_bytes(1, 'big') + (len(dados)).to_bytes(1, 'big')+ b'\x00\x00\x00\x00\x00' + calculator.checksum(dados).to_bytes(1, 'big')
+                payload_1 = dados
+                dtg = head+payload_1+ceop
+            elif tipo == 5: # mensagem de time out
+                head = b'\x05\x00'+ b'\x00\x00\x00\x00\x00\x00\x00\x00'
+                dtg = head+ceop
+            return dtg
         
-        # Ativa comunicacao. Inicia os threads e a comunicação seiral 
+        print("Iniciou o main")
+        com1 = enlace(serialName)
         com1.enable()
 
         time.sleep(.2)
         com1.sendData(b'00')
         time.sleep(1)
-        #Se chegamos até aqui, a comunicação foi aberta com sucesso. Faça um print para informar.
-        print("Abriu a comunicação")
 
+        print("Abriu a comunicação")
         image1 = "./img/1.png" #img a ser transmitida
         image2 = "./img/2.png" 
 
@@ -83,73 +79,80 @@ def main():
         txBuffer1 = open(image1,"rb").read() #imagem em bytes!
         txBuffer2 = open(image2,"rb").read() #imagem em bytes!
         print("Imagem carregada")
+
         numero_img = 1
-        while numero_img<=2:
-            com1.sendData(datagrama(1, 0, divide_pacotes(txBuffer1)[1], 0, numero_img)) #envia o convite para o servidor (handshake)
-            escrever_log(f"Convite enviado para o servidor.", "log_cliente.txt")
-            h,_ = com1.getData(10) #pega o head do server pra ver se ele aceitou o convite
-            print(h)
-            ceop,_ =com1.getData(4) #pega o ceop
-            if h[0] == 2:
-                escrever_log(f"Tem-se início o envio do arquivo de extensâo {numero_img}", "log_cliente.txt")
+        com1.sendData(datagrama(1, 0, divide_pacotes(txBuffer1)[1], 0, numero_img)) #envia o convite para o servidor (handshake)
+        escrever_log(f"Convite enviado para o servidor.", "log_cliente.txt")
+        h,_ = com1.getData(10) #pega o head do server pra ver se ele aceitou o convite
+        print(h)
+        ceop,_ =com1.getData(4) #pega o ceop
+        ultimo = True
+        if h[0] == 2: 
+            print("Server aceitou o convite")
+            while numero_img<=2:
+                escrever_log(f"Tem-se início o envio do arquivo {numero_img}.png", "log_cliente.txt")
                 com1.sendData(datagrama(3,1,divide_pacotes(txBuffer1)[1],divide_pacotes(txBuffer1)[0][0], numero_img)) #envia o primeiro pacote
-                tempo_inicial=time.time()
-                escrever_log("Pacote 1 enviado.", "log_cliente.txt")
-                print("Pacote 1 enviado.")
+                print("Pacote 1 enviado!")
+                escrever_log(f"Pacote 1 enviado.", "log_cliente.txt")
+                tempo_inicial = time.time()
                 timeout = 10
                 tempo_duracao = 0
-                i = 2
+                i = 1
                 while i <= (divide_pacotes(txBuffer1)[1]):
-                    print ("teste", i)
-                    while com1.rx.getBufferLen() == 0:
+                    while com1.rx.getBufferLen() < 14:
                         time.sleep(0.02)
                         tempo_duracao = time.time() - tempo_inicial
                         if tempo_duracao >= 1:
-                            # print(tempo_duracao)
-                            time.sleep(2)
+                            time.sleep(.5)
+                            print("Reenviando confirmação de recebimento do pacote")
+                            print(numero_img)
+                            print(i)
                             com1.sendData(datagrama(3, i-1, divide_pacotes(txBuffer1)[1], divide_pacotes(txBuffer1)[0][i-2], numero_img)) #envia o proximo pacote
+                            tempo_inicial = time.time()
+
                     head,_ = com1.getData(10) #pega o head
                     ceop,_ =com1.getData(4) #pega o ceop
+                    i = head[1]
                     print("head que server mandou pro client: ", head)
 
                     if head[0] == 5:
                         escrever_log("Time out.", "log_cliente.txt")
                         print("Time out")
-                        com1.disable()
                         break
+
                     elif head[0] == 6:
+                        print(head[1])
                         escrever_log(f"Número do pacote esperado incorreto. Enviando novamente o pacote requisitado", "log_cliente.txt")
                         com1.sendData(datagrama(3, head[1], divide_pacotes(txBuffer1)[1], divide_pacotes(txBuffer1)[0][head[1]-1], numero_img))
+                        print("pacote a ser reenviado, tipo 6",datagrama(3, head[1], divide_pacotes(txBuffer1)[1], divide_pacotes(txBuffer1)[0][head[1]-1], numero_img))
 
                     elif head[0] == 7:
                         escrever_log(f"Erro na transmissao do pacote {head[1]}. Reenviá-lo.", "log_cliente.txt")
-                        com1.sendData(datagrama(3, head[1], divide_pacotes(txBuffer1)[1], divide_pacotes(txBuffer1)[0][head[1]], numero_img))
+                        com1.sendData(datagrama(3, head[1], divide_pacotes(txBuffer1)[1], divide_pacotes(txBuffer1)[0][head[1]-1], numero_img))
 
                     elif head[0] == 4:
-                        if i == divide_pacotes(txBuffer1)[1]:
-                            print("contador é igual ao numero de pacotes")
-                            com1.sendData(datagrama(3, i, divide_pacotes(txBuffer1)[1], divide_pacotes(txBuffer1)[0][i-1], numero_img)) #envia o proximo pacote
-                            head,_ = com1.getData(10)
-                            ceop,_ = com1.getData(4)
-                            print(f"ultimo head mandado pelo server: ", head)
-                            escrever_log(f"Envio do arquivo de extensão {numero_img} finalizado.", "log_cliente.txt")   
+                        if head[1] == 255:
+                            escrever_log(f"Envio do arquivo {numero_img}.png finalizado.", "log_cliente.txt")   
+                            ultimo = True
+                            com1.disable()
+                            print("Comunicação encerrada")
+                            sys.exit()
+
+                        elif i == divide_pacotes(txBuffer1)[1] and ultimo:
+                            print("contador é igual ao numero de pacotes") 
+                            print("enviando o ultimo pacote")
+                            com1.sendData(datagrama(3, head[1], divide_pacotes(txBuffer1)[1], divide_pacotes(txBuffer1)[0][head[1]-1], numero_img)) #envia o proximo pacote
+                            tempo_inicial=time.time()
+                            ultimo = False
                             numero_img += 1
-                            txBuffer1 = open(image2,"rb").read() #imagem em bytes
-                            i += 1 
-                        else:
+
+                        elif i != divide_pacotes(txBuffer1)[1]:
                             tempo_inicial = time.time()
-                            com1.sendData(datagrama(3, i, divide_pacotes(txBuffer1)[1], divide_pacotes(txBuffer1)[0][i-1], numero_img)) #envia o proximo pacote
+                            com1.sendData(datagrama(3, head[1], divide_pacotes(txBuffer1)[1], divide_pacotes(txBuffer1)[0][head[1]-1], numero_img)) #envia o proximo pacote
+                            time.sleep(.5)
                             print("Pacote ", i)
+                            print("pacote a ser enviado",datagrama(3, head[1], divide_pacotes(txBuffer1)[1], divide_pacotes(txBuffer1)[0][head[1]-1], numero_img))
                             escrever_log(f"Pacote {i} enviado.", "log_cliente.txt")
-                            i += 1
-
-                if time.time() - tempo_inicial > timeout:
-                    escrever_log("Time out.", "log_cliente.txt")
-                    print("Tempo de envio dos arquivos: ", time.time() - tempo_inicial)
-                    com1.sendData(datagrama(5, 0, 0, 0, 0))
-                    break
-                    com1.disable()
-
 
         print("Razão entre os tamanhos dos arquivos: ", len(txBuffer1)/len(txBuffer2))  
         
@@ -165,10 +168,7 @@ def main():
         com1.disable() 
         
 
-    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
 if __name__ == "__main__":
     main()
 
 
-
-# Se durante a transmissão os fios que conectam os arduinos forem desconectados e reconectados, a transmissão deve continuar após a reconexão.
